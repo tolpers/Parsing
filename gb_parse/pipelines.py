ee268f4869c7ee39fa4887b8d9cb34531442d52d
mr.Prusakov <tolpers@ya.ru>
2021-04-20 14:48:13 +0300

    Add homework to lesson_6

--- gb_parse/pipelines.py
+++ gb_parse/pipelines.py
@@ -6,6 +6,8 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from scrapy import Request
+from scrapy.pipelines.images import ImagesPipeline
 import pymongo
 
 
@@ -21,4 +23,15 @@ class GbParseMongoPipeline:
 
     def process_item(self, item, spider):
         self.db[spider.name].insert_one(item)
+        return item
+
+
+class GbImageDownloadPipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        for url in item.get("photos", []):
+            yield Request(url)
+
+    def item_completed(self, results, item, info):
+        if "photos" in item:
+            item["photos"] = [itm[1] for itm in results]
         return item
--- gb_parse/settings.py
+++ gb_parse/settings.py
@@ -27,7 +27,7 @@ CONCURRENT_REQUESTS = 32
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 1
+
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 16
 # CONCURRENT_REQUESTS_PER_IP = 16
@@ -52,9 +52,11 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#    'gb_parse.middlewares.GbParseDownloaderMiddleware': 543,
-# }
+DOWNLOADER_MIDDLEWARES = {
+    # 'gb_parse.middlewares.GbParseDownloaderMiddleware': 543,
+    "rotating_proxies.middlewares.RotatingProxyMiddleware": 610,
+    "rotating_proxies.middlewares.BanDetectionMiddleware": 620,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
@@ -64,23 +66,28 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+IMAGES_STORE = "images"
+
 ITEM_PIPELINES = {
     "gb_parse.pipelines.GbParsePipeline": 100,
     "gb_parse.pipelines.GbParseMongoPipeline": 200,
+    "gb_parse.pipelines.GbImageDownloadPipeline": 50,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-# AUTOTHROTTLE_ENABLED = True
+DOWNLOAD_DELAY = 0.05
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-# AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 3
 # The maximum download delay to be set in case of high latencies
-# AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 10
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
 # AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-# AUTOTHROTTLE_DEBUG = False
+AUTOTHROTTLE_DEBUG = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
@@ -89,3 +96,6 @@ ITEM_PIPELINES = {
 # HTTPCACHE_DIR = 'httpcache'
 # HTTPCACHE_IGNORE_HTTP_CODES = []
 # HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+ROTATING_PROXY_LIST_PATH = (
+    "/Users/tolpe/PyProject/06. Parsing/proxies"
+)
